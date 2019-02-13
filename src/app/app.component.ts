6788aea11b40b67fe9ab98f841fcdff8ca3d605f
Bhawna Pandey <bhawna.p@healthsignz.com>
2019-02-13 20:10:11 +0530

    13Feb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+    `
 })
 export class AppComponent {
-  title = 'Praveen - Tour of Heroes';
+  title = 'Tour of Heroes';
+  myHero = 'Windstorm';
 }
